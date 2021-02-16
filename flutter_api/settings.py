cbc0aa16a4cb7d64f75e72e55dc1197bc42f49ca
semoslar <cem.altuner1997@gmail.com>
2021-02-16 23:00:06 +0300

    static files uplload

--- flutter_api/settings.py
+++ flutter_api/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # -------------- MY CHANGES --------------
 
-STATIC_ROOT = '/home/ubuntu/DjangoBackend/static/'
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 AUTH_USER_MODEL = 'core.Customer'
