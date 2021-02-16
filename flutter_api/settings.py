a59effbb85dcfda7a55e23d7f434a75578ab0c2e
semoslar <cem.altuner1997@gmail.com>
2021-02-16 23:01:44 +0300

    debug false

--- flutter_api/settings.py
+++ flutter_api/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8$loaim80a2&&^_hkttnczv-=^d-8*_$a-f5_0z*%ixjk+vxx6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
